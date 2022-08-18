9a992ec6956e0c2893344aca694a59ced21c6370
jaydeep-helpshift <jaydeep@helpshift.com>
2022-08-18 15:08:14 +0530

    Initial Commit

---
+++ HS Migrator/HS_MigratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HS_MigratorApp.swift
+//  HS Migrator
+//
+//  Created by Jaydeep Joshi on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct HS_MigratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
